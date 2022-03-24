bee11ac88b13af773fa4f920184dd1a99411599e
Serhii Teperechkin <67683335+SerhiiTeperechkin@users.noreply.github.com>
2022-03-24 23:22:30 +0200

    Create memoized_fibonacci.py

---
+++ 5 kuy/memoized_fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    chache = {}
+    def fibonacci_memoized(m):
+        assert m >= 0
+        if m in [0,1]:
+            return m
+        if m in chache:
+            return chache[m]
+        result = fibonacci_memoized(m - 1) + fibonacci_memoized(m - 2)
+        chache[m] = result
+        return result
+    return fibonacci_memoized(n)
